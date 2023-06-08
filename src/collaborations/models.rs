7b40c2e861654b9bdcd9c883eacf234192a2530b
barduinor <barduinor@gmail.com>
2023-06-08 16:15:53 -0400

    users models

---
+++ src/collaborations/models.rs
@@ -0,0 +1,2 @@
+pub mod collaborations_all_of_order;
+pub use self::collaborations_all_of_order::CollaborationsAllOfOrder;
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,10 @@
+#[macro_use]
+extern crate serde_derive;
+
+extern crate serde;
+extern crate serde_json;
+
 pub mod api;
 pub mod auth;
+pub mod collaborations;
+pub mod users;
